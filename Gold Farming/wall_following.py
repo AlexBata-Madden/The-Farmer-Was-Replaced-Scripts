9db4ef6fbfc0b32465aa4fa828a5210544f1c3a2
Alex <alexbatamadden@gmail.com>
2026-06-04 01:21:06 +0100

    refactored even more

--- Gold Farming/wall_following.py
+++ Gold Farming/wall_following.py
@@ -3,37 +3,19 @@ def generate_full_map_maze():
 	plant(Entities.Bush)
 	use_item(Items.Weird_Substance, get_world_size() * 2**(num_unlocked(Unlocks.Mazes) - 1))
 
-
-def rotate(d):
-	first = d[0]
-	d.remove(first)
-	d.append(first)
-
-
-def rotate_n(d, n):
-	for i in range(n):
-		rotate(d)
-
-
-def wall_follow_step(directions):
-	# directions = [left, forward, right, back]
-	rotations = [3, 0, 1, 2]
-
-	for i in range(4):
-		if can_move(directions[i]):
-			move(directions[i])
-			rotate_n(directions, rotations[i])
-			return
-
-
 generate_full_map_maze()
 
 directions = [West, North, East, South]
 
 while True:
-	if get_entity_type() == Entities.Treasure:
-		harvest()
-		generate_full_map_maze()
-		directions = [West, North, East, South]
-
-	wall_follow_step(directions)
+	heading = 0
+	
+	while get_entity_type() != Entities.Treasure:
+		
+		heading = (heading - 1) % 4
+		
+		while not move(directions[heading]):
+			heading = (heading + 1) % 4
+		
+	harvest()
+	generate_full_map_maze()
