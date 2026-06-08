615d7ff7677ccda42fb77cbcc900b8523a6c63e1
Alex <alexbatamadden@gmail.com>
2026-06-08 12:58:16 +0100

    simplify hard_farm.py

--- Hay Farming/hay_farm.py
+++ Hay Farming/hay_farm.py
@@ -1,14 +1,10 @@
 def farm_hay():
 	
 	def hay_worker():
-		while True:
-			for _ in range(get_world_size()):
-				x = get_pos_x()
-				y = get_pos_y()
-	
-				if(can_harvest()):
-					harvest()
-				move(North)
+		while True:	
+			if(can_harvest()):
+				harvest()
+			move(North)
 
 	clear()
 	
